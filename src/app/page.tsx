4bbd620c1aec6d754c58f2ada9686909d869219b
eguzki <eaguirre@netflix.com>
2023-12-16 01:49:19 -0600

    basic layout, reads csv file and downloads template

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,57 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client';
+
+import { downloadStringAsFile } from '@/util/downloadStringAsFile';
+import { Typography, Button, Box } from '@mui/material';
+import { parse } from 'csv-parse/sync';
+import { ChangeEvent } from 'react';
 
 export default function Home() {
+  function handleFileInputChange (event: ChangeEvent<HTMLInputElement>) {
+    const file = event.target.files?.[0]
+
+    if (!file) return 'No files found'
+
+    const reader = new FileReader()
+
+    reader.onload = async (e: ProgressEvent<FileReader>) => {
+      const text = e.target?.result as string;
+      console.log(text)
+      let parsedData = parse(text, { columns: true });
+      console.log(parsedData);
+    };
+
+    reader.readAsText(file);
+  }
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+    <Box padding={6} display='flex' flexDirection='column' gap={1}>
+      <Typography variant='h4'>
+        Lets sign up some leads in skillmap
+      </Typography>
+      <Typography variant='body1'>
+        Upload your leads in .csv format below 👇
+      </Typography>
+      <Box display='flex' gap={1} paddingTop={2}>
+        <Button
+          variant="contained"
+          component="label"
         >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+          Upload CSV
+          <input
+            type="file"
+            accept=".csv"
+            onChange={handleFileInputChange}
+            hidden
+          />
+        </Button>
+        <Button
+          variant="outlined"
+          onClick={() => downloadStringAsFile('email,firstName,lastName\ntest-jane-doe@gmail.com,Jane,Doe', 'lead-data-template.csv')}
         >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+          Download CSV Template
+          
+        </Button>
+      </Box>
+    </Box>
   )
 }
