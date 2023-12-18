54fba063bfb695e85b6001a8bfe8bf57e86eefb6
eguzki <eaguirre@netflix.com>
2023-12-17 22:12:14 -0600

    parse and render csv in table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 'use client';
 
+import { Lead, LeadSchema } from '@/types/models';
 import { downloadStringAsFile } from '@/util/downloadStringAsFile';
-import { Typography, Button, Box } from '@mui/material';
+import { Typography, Button, Box, TableContainer, Paper, Table, TableHead, TableRow, TableCell, TableBody } from '@mui/material';
 import { parse } from 'csv-parse/sync';
-import { ChangeEvent } from 'react';
+import { ChangeEvent, useState } from 'react';
 
 export default function Home() {
-  function handleFileInputChange (event: ChangeEvent<HTMLInputElement>) {
+  const [leads, setLeads] = useState<Lead[] | null>(null)
+  const [invalidLeadsCount, setInvalidLeadsCount] = useState<number | null>(null)
+
+  function handleFileInputChange(event: ChangeEvent<HTMLInputElement>) {
     const file = event.target.files?.[0]
 
     if (!file) return 'No files found'
@@ -15,9 +19,24 @@ export default function Home() {
 
     reader.onload = async (e: ProgressEvent<FileReader>) => {
       const text = e.target?.result as string;
-      console.log(text)
-      let parsedData = parse(text, { columns: true });
-      console.log(parsedData);
+      const data = parse(text, { columns: true }) as unknown[];
+
+      const validLeads: Lead[] = []
+      let invalidCount: number = 0;
+
+      data.forEach((lead) => {
+        const parsed = LeadSchema.safeParse(lead);
+
+        if (!parsed.success) {
+          invalidCount += 1
+          return;
+        }
+
+        validLeads.push(parsed.data)
+      })
+
+      setLeads(validLeads)
+      setInvalidLeadsCount(invalidLeadsCount)
     };
 
     reader.readAsText(file);
@@ -49,9 +68,29 @@ export default function Home() {
           onClick={() => downloadStringAsFile('email,firstName,lastName\ntest-jane-doe@gmail.com,Jane,Doe', 'lead-data-template.csv')}
         >
           Download CSV Template
-          
+
         </Button>
       </Box>
+      <TableContainer>
+        <Table size='small' aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Email</TableCell>
+              <TableCell>First Name</TableCell>
+              <TableCell>Last Name</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {leads?.map((lead) => (
+              <TableRow key={lead.email}>
+                <TableCell>{lead.email}</TableCell>
+                <TableCell>{lead.firstName}</TableCell>
+                <TableCell>{lead.lastName}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Box>
   )
 }
